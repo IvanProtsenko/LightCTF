77234002195033c2c6010c48518c481d571041f0
IvanProtsenko <ivnprotsenko@gmail.com>
2018-04-15 17:52:39 +0300

    Update scoreboard.blade.php

--- resources/views/scoreboard.blade.php
+++ resources/views/scoreboard.blade.php
@@ -5,9 +5,14 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+                @php
+                $id = 0;
+                @endphp
                 @foreach($users->sortByDesc('score') as $user)
-
                     <div class="card mb-3">
+                        <div class="card-header">
+                            <h2>{{++$id}}</h2>
+                        </div>
                         <div class="card-body">
                             <h2>Name: {{$user->name}}</h2>
                             <h5>Score: {{$user->score}}</h5>
